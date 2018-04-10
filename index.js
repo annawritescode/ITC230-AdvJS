611c226e253c1663819bd0fe9a0475239bf734ee
Anna Wong <anna.y.wong01@gmail.com>
2018-04-10 22:04:29 +0000

    third commit didn't save

--- index.js
+++ index.js
@@ -8,6 +8,23 @@ http.createServer(function (req, res) {
     var path = req.url.toLowerCase();
     
     // Using switch for Path
+       switch(path) {
+      
+        case'':
+          fs.readFile(res,'ITC230-AdvJS/home.html', 'text/html');
+          break;
+    
+        case'/about':
+          fs.readFile(res,'ITC230/about.html',' text/html');
+           res.writeHead(200, {'Content-Type': 'text/plain'});
+           res.end('About Page');
+           break;
+       default:
+           res.writeHead(404, {'Content-Type': 'text/plain'});
+           res.end('404:Page not found.');
+           break;
+  }
+
 
 }).listen(process.env.PORT, process.env.IP);
 //console.log('Server running at http://127.0.0.1:8081/');
