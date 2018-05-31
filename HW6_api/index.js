a2703fb61111520976313116fc542f349a010c7e
Anna Wong <anna.y.wong01@gmail.com>
2018-05-31 01:35:28 +0000

    final commit assig 6 routes

--- HW6_api/index.js
+++ HW6_api/index.js
@@ -37,13 +37,14 @@ app.get('/about', (req, res) => {
 
 
 // return all records
-app.get('/api/film/', (req, res) => {
+// test url https://2563718b9050453e88f8755bff79bc10.vfs.cloud9.us-east-2.amazonaws.com/api/film/
+app.get('/api/titles/', (req, res) => {
  // return all records
-Film.find({}, (err, film) => {
+Film.find({}, (err, titles) => {
      //console.log(err);
     //console.log(film);
    if (err) return (err);
-   res.json(film);
+   res.json(titles);
   
  });
 });
@@ -58,13 +59,15 @@ app.post('/get', (req,res) => {
 });
 
 //find specific record
-app.get('/api/film/:title', (req,res,next) => {
+app.get('/api/title/:title', (req,res,next) => {
   //test console.log(req.query.title);
-  Film.findOne({title:req.params.title.toLowerCase()}, (err, film) => {
+  //allows to find title in upper and lower case
+  const filmTitle= new RegExp(req.params.title,'i');
+  Film.findOne({title:filmTitle}, (err, title) => {
      //console.log(err);
     //console.log(film);
    if (err) return next(err);
-   res.json(film);
+   res.json(title);
    // other code here
  });
 });
@@ -83,6 +86,7 @@ app.post('/get', (req,res) => {
 // 
 
 // delete item
+// test url https://2563718b9050453e88f8755bff79bc10.vfs.cloud9.us-east-2.amazonaws.com/api/delete/amelia
 app.get('/api/delete/:title', (req,res,next) => { 
 Film.remove({title:req.params.title.toLowerCase()}, (err, title) => {
      //console.log(err);
@@ -93,6 +97,8 @@ Film.remove({title:req.params.title.toLowerCase()}, (err, title) => {
 });
 });
 
+// add an item to database
+// test url https://2563718b9050453e88f8755bff79bc10.vfs.cloud9.us-east-2.amazonaws.com/api/film/add/movie2/dude/2018
 
 app.get('/api/film/add/:title/:director/:releaseDate', (req, res) => {
     let obj={ title:req.params.title, director:req.params.director,releaseDate:req.params.releaseDate};
